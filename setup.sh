4d75158d5a5558560fbb458e7ad299b914cafdfb
SatooRu65536 <ryuto4619@gmail.com>
2022-11-04 13:56:03 +0900

    :tada: init

---
+++ setup.sh
@@ -0,0 +1,5 @@
+# イラレショートカット
+mv illustrator-shortcut.kys ~/Library/Preferences/Adobe\ Illustrator\ 27\ Settings/ja_JP/
+
+# イラレワークスペース
+mv illustrator-workspace /Users/satooru/Library/Preferences/Adobe\ Illustrator\ 27\ Settings/ja_JP/ワークスペース/
